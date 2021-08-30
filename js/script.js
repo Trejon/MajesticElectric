60f847754a8f2bfc1e51fdf49bfefa433cd1d387
Trejon Stallsworth <trejonstallsworth@gmail.com>
2021-08-29 22:12:44 -0600

    update

--- js/script.js
+++ js/script.js
@@ -15,4 +15,43 @@ $(document).ready(function() {
         startDelay: 1000,
         showCursor: false
     })
+
+
+
+
+
+    // $(".owl-carousel").owlCarousel({
+    //     loop: true,
+    //     items: 4,
+    //     autoplay: true,
+    //     autoplayTimeout: 2e3,
+    //     autoplayHoverPause: false,
+    //     responsive: {
+    //         0: { items: 1 },
+    //         480: { items: 3 },
+    //         768: { items: 5 },
+    //         938: { items: 6 }
+    //     }
+    // });
+
+
+    $('.owl-carousel').owlCarousel({
+        items: 2,
+        loop: true,
+        margin: 50,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true
+    });
+    $('.play').on('click', function() {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function() {
+        owl.trigger('stop.owl.autoplay')
+    })
+
+
+
 })
